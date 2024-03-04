34ce0bcd106332435ac011753219cb5734207597
Kira <thiennguyen.dev.0103@gmail.com>
2024-03-04 14:49:47 +0700

    feat: config swagger

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,12 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
+import { ReservationsModule } from './reservations.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Reservation')
+    .setDescription('The reservation API')
+    .setVersion('1.0')
+    .addTag('reservations')
+    .build();
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+  app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
