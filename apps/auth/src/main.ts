dfe678ea8c195c8b2d6cd7e5d5ea025520d19f5d
Kira <thiennguyen.dev.0103@gmail.com>
2024-03-05 21:21:07 +0700

    feat: implement auth service

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+import { AuthModule } from './auth.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AuthModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+  app.setGlobalPrefix('api');
+  await app.listen(3001);
+}
+bootstrap();
